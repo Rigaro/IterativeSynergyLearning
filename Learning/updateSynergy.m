9df1be6bea2e00ee35e0faa041fececb256ec64d
Ricardo Garcia Rosas <ricardog@student.unimelb.edu.au>
2017-09-04 10:29:42 +1000

    Added basic learning functions.

---
+++ Learning/updateSynergy.m
@@ -0,0 +1,18 @@
+if(i<2)
+    alpha = alphaH(i) - 0.1;
+    i = i + 1;
+else
+    dJ = JH(i)-JH(i-1);
+    dalpha = alphaH(i)-alphaH(i-1);
+    grad = dJ/dalpha;
+    if(grad>10)
+        grad = 10;
+    elseif(grad<-10)
+        grad = -10;
+    end
+    alpha = alphaH(i) - 0.5*grad;     % 2 for low qs variation
+    i = i + 1;
+end
+i
+alphaH(i) = alpha;
+% set_param('sim2DOFArmSynergySimulink/Synergy/alpha','Gain',num2str(alphaH(i)));
