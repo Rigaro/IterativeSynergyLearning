949d85bc42aec1a496ffe57ef5a30f6c4c471991
Ricardo Garcia Rosas <ricardog@student.unimelb.edu.au>
2017-09-05 09:31:38 +1000

    Added information to experiment historical data. Added a script for plotting experiment/test results.

--- Management/Experiment/appendIteration.m
+++ Management/Experiment/appendIteration.m
@@ -1,9 +1,20 @@
+%
+% Append iteration for 1DOF simulation
+%
 % Hand realted
-pH(:,i) = simOut.p(:,:,end);   % Historic position data
+pfH(:,i) = simOut.p(:,:,end);   % Historic end position data
+for j=1:length(simOut.tout)
+    xPosH(j,i) = simOut.p(1,1,j);
+    yPosH(j,i) = simOut.p(2,1,j);
+end
 % Joint related
-% q_sH(:,i) = simOut.q_s;        % Historic shoulder position data
-% q_eH(:,i) = simOut.q_e;        % Historic elbow position data
+for j=1:length(simOut.q_s)
+    q_sH(j,i) = simOut.q_s(j);        % Historic shoulder position data
+end
+for j=1:length(simOut.q_e)
+    q_eH(j,i) = simOut.q_e(j);        % Historic elbow position data
+end
 % Learning realted
 % alphaH(i) = str2double(get_param('sim2DOFArmSynergySimulink/Synergy/alpha','Gain'));
 % alphaH(i) = alpha;
-JH(i) = cost(p_f,pH(:,i));            % Historic cost data
+JH(i) = cost(p_f,pfH(:,i));            % Historic cost data
---
+++ Results/Utilities/testPlot.m
@@ -0,0 +1,46 @@
+% Plot alpha values
+figure(1);
+plot(alphaH);
+title('Synergy gain v. iteration');
+xlabel('iteration');
+ylabel('\alpha');
+% Plot cost values
+figure(2);
+plot(JH);
+title('Cost v. iteration');
+xlabel('iteration');
+ylabel('Cost');
+% Plot q_s
+figure(3);
+plot(simOut.q_s);
+title('Sample shoulder trajectory');
+xlabel('sample');
+ylabel('q_s (rad)');
+% Plot q_e
+figure(4);
+plot(simOut.q_e);
+title('Sample elbow trajectory');
+xlabel('sample');
+ylabel('q_e (rad)');
+% Plot pf(i)
+figure(5);
+plot(pfH(1,:),pfH(2,:),'x');
+hold on
+plot(p_f(1),p_f(2),'ro');
+hold off
+title('Hand end positions');
+xlabel('x (m)');
+ylabel('y (m)');
+% Plot sample p
+for j=1:length(simOut.tout)
+    xPos(j) = simOut.p(1,1,j);
+    yPos(j) = simOut.p(2,1,j);
+end
+figure(6);
+plot(xPos,yPos);
+hold on
+plot(p_f(1),p_f(2),'ro');
+hold off
+title('Sample hand trajectory');
+xlabel('x (m)');
+ylabel('y (m)');
