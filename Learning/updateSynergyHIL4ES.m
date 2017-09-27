ab707f17ee926335e985d21ceb201aa60c889701
Ricardo Garcia Rosas <ricardog@student.unimelb.edu.au>
2017-09-27 18:47:07 +1000

    Simplified synergy update code.

--- Learning/updateSynergyHIL4ES.m
+++ Learning/updateSynergyHIL4ES.m
@@ -6,18 +6,20 @@ else
     % High pass filter
     v(i) = -(h*v(i-1))+(JH(i)-JH(i-1));
     
-    % Estimation dither
-    for j=1:3        
-        gradJ(j) = aDither(j)*cos(wDither(j)*i)*v(i);
-    end
+    % Demodulation
+%     for j=1:3        
+%         gradJ(j) = aDither(j)*cos(wDither(j)*i)*v(i);
+%     end
+    gradJ = aDither.*cos(wDither*i).*v(i);
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%%%%% Gradient descent %%%%%%%%%
     % Gradient
     theta = thetaH(i,:) + gamma*gradJ;
-    % Gradient dither
-    for j=1:3
-        theta(j) = theta(j) + aDither(j)*cos(wDither(j)*i);
-    end
+    % Perturbation dither
+%     for j=1:3
+%         theta(j) = theta(j) + aDither(j)*cos(wDither(j)*i);
+%     end
+    theta = theta + aDither.*cos(wDither*i);
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     i = i + 1;
 end
